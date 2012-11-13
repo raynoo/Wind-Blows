579cd00e98829e24a43c436189a5547dac2d03df
Renu Srinivasan <raynoo@gmail.com>
2012-11-13 15:30:26 -0600

    basic files

--- src/p4/wind/main/Main.java
+++ src/p4/wind/main/Main.java
@@ -1,8 +1,23 @@
 package p4.wind.main;
 
-public class Main {
+import omicronAPI.OmicronAPI;
+import p4.wind.application.AppConstants;
+import processing.core.PApplet;
 
-	public Main() {
-		// TODO Auto-generated constructor stub
+public class Main extends PApplet {
+
+	static public void main(String args[]) {
+	    PApplet.main(new String[] { "main.Program" });
+    }
+	
+	OmicronAPI omicronManager;
+	
+	public void setup() {
+		this.size(AppConstants.fullScreenWidth, AppConstants.fullScreenHeight);
+		this.background(0, 0, 0);
+	}
+	
+	public void draw() {
+		
 	}
 }
