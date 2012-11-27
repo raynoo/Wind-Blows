8c8072975e9916e1d74031771ff5c80a604ebebe
Gaurav Purandare <gpuran2@uic.edu>
2012-11-26 22:30:16 -0600

    Slider sketch added. I am going to add the play, pause, stop etc buttons.

--- WindBlows/src/cs424/windblows/listeners/FilterListener.java
+++ WindBlows/src/cs424/windblows/listeners/FilterListener.java
@@ -1,6 +1,9 @@
 package cs424.windblows.listeners;
 
+import java.util.Date;
+
 public interface FilterListener {
 	public void categoryAdded(int categoryId);
 	public void categoryRemoved(int categoryId);
+	public void dateChanged(Date date);
 }
